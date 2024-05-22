75b7629fe6cf5047eb87e8e1fcc144920616a419
benjaminchengit <B.Chen-7@student.tudelft.nl>
2024-05-22 16:04:40 +0200

    Code final 5/22

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from rocketcea.cea_obj import CEA_Obj, add_new_fuel, add_new_oxidizer, add_new_propellant
 import CoolProp.CoolProp as CP
 from pylab import *
+import numpy as np
 
 # isp_units            = 'sec',         # N-s/kg, m/s, km/s
 # cstar_units          = 'ft/sec',      # m/s
@@ -11,13 +12,7 @@ from pylab import *
 # density_units        = 'lbm/cuft',    # g/cc, sg, kg/m^3
 # specific_heat_units  = 'BTU/lbm degR' # kJ/kg-K, cal/g-C, J/kg-K
 # viscosity_units      = 'millipoise'   # lbf-sec/sqin, lbf-sec/sqft, lbm/ft-sec, poise, centipoise
-# thermal_cond_units   = 'mcal/cm-K-s'  # millical/cm-degK-sec, BTU/hr-ft-degF, BTU/s-in-degF,
-#                                       #  cal/s-cm-degC, W/cm-degC
-
-# # O/F ratio
-# C = CEA_Obj( oxName='LOX', fuelName='LH2')
-# for mr in range(2,9):
-#     print(mr, C.get_Isp(Pc=100.0, MR=mr, eps=40.0) )
+# thermal_cond_units   = 'mcal/cm-K-s'  # millical/cm-degK-sec, BTU/hr-ft-degF, BTU/s-in-degF, cal/s-cm-degC, W/cm-degC
 
 
 
@@ -30,47 +25,59 @@ add_new_fuel( 'LCO', card_str )
 
 
 # # General information
-ispObj = CEA_Obj(oxName='LOX', fuelName='LCO')
-# s = ispObj.get_full_cea_output( Pc=30, MR=0.571, eps=40.0, short_output=1, pc_units='bar', output='siunits')
-# print( s )
+ispObj = CEA_Obj(propName='', oxName='LOX', fuelName='LCO')
+s = ispObj.get_full_cea_output( Pc=30, MR=0.45, eps=22.8, short_output=1, pc_units='bar', output='siunits')
+print( s )
 
 
+# C* vs O/F
+pcL = np.array([5.3, 10.7, 30]) #bar
+pcL = pcL * 14.5037738        #psi
+for Pc in pcL:
+    cstarArr = []
+    mrArr = []
+    MR = 0.25
+    while MR < 2:
+        cstarArr.append( ispObj.get_Cstar( Pc=Pc, MR=MR ) * 0.3048 )
+        mrArr.append(MR)
+        MR += 0.01
+    Pc = Pc / 14.5037738
+    plot(mrArr, cstarArr, label='Pc=%g bar' %Pc)
 
-# # C* vs O/F
-# pcL = [ 435.113213 ]
-# # ispObj = CEA_Obj(propName='', oxName='LOX', fuelName='LCO')
-#
-# for Pc in pcL:
-#     cstarArr = []
-#     MR = 0.25
-#     mrArr = []
-#     while MR < 2:
-#         cstarArr.append( ispObj.get_Cstar( Pc=Pc, MR=MR ) * 0.3048 )
-#         mrArr.append(MR)
-#         MR += 0.01
-#     Pc = 30
-#     plot(mrArr, cstarArr, label='Pc=%g bar' %Pc)
-# ylim(1100, 1400)
-# xlim(0, 2.2)
-# legend(loc='best')
-# grid(True)
-# title( ispObj.desc )
-# xlabel( 'O/F Ratio' )
-# ylabel( 'Cstar (m/s)' )
-# # savefig('cea_cstar_plot.png', dpi=120)
-# show()
-
-
-
+    # plot max point
+    bbox_props = dict(boxstyle="square,pad=0.0001", fc="w", ec="k", lw=0.72)
+    arrowprops = dict(arrowstyle="->")
+    kw = dict(arrowprops=arrowprops, bbox=bbox_props)
+    xmax = mrArr[np.argmax(cstarArr)]
+    ymax = np.max(cstarArr)
+    annotate(f'{round(xmax, 2), int(ymax)}', xy=(xmax, ymax), xytext=(xmax + 0.8 , ymax - 5), **kw)
+ylim(1100, 1400)
+xlim(0, 2.2)
+legend(loc='lower left')
+grid(True)
+title( ispObj.desc )
+xlabel( 'O/F Ratio' )
+ylabel( 'Cstar (m/s)' )
+# savefig('cea_cstar_plot.png', dpi=120)
+show()
 
 
 # Isp vs eps
 ispArr = []
-epsL = range(1,201,1)
+isp_theo_Arr = []
+epsL = range(1,271,1)
 for eps in epsL:
-    ispArr.append( ispObj.get_IvacCstrTc_ThtMwGam(Pc=435.113213, MR=0.571, eps=eps)[0] )
+    ispArr.append( ispObj.get_IvacCstrTc_ThtMwGam(Pc=435.113213, MR=0.45, eps=eps)[0] )
+    isp_theo_Arr.append(ispObj.get_IvacCstrTc_ThtMwGam(Pc=435.113213, MR=0.571, eps=eps)[0])
 
-plot(epsL, ispArr, label='Pc=30 bar; O/F=0.571')
+plot(epsL, ispArr, label='Pc=30 bar; O/F=0.45')
+plot(epsL, isp_theo_Arr, label='Pc=30 bar; O/F=0.571')
+plt.plot([22.7981, 22.7981], [150, 270], 'r--',label='Isp=270s, Ae/At=22.8')
+plt.plot([0, 22.7981],[270,270],'r--')
+plt.plot([262, 262], [150, 308], 'g--',label='Isp=308s, Ae/At=262')
+plt.plot([0, 262],[308.07, 308.07],'g--')
+ylim(150, 400)
+xlim(0, 270)
 legend(loc='best')
 grid(True)
 title( ispObj.desc )
@@ -79,4 +86,8 @@ ylabel( 'Isp' )
 show()
 
 
-# print( ispObj.get_eps_at_PcOvPe(Pc=435.113213, MR=0.5, PcOvPe=(30*10**5)/700, frozen=0, frozenAtThroat=0) )
+# # O/F ratio
+# for mr in range(2,9):
+#     print(mr, ispObj.get_Isp(Pc=435.113213, MR=mr, eps=40) )
+
+# print( ispObj.get_eps_at_PcOvPe(Pc=435.113213, MR=0.5, PcOvPe=435.113213/0.1015, frozen=1, frozenAtThroat=1) )
