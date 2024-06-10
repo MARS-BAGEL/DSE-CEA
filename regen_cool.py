a9cb9005079d0380bb90fe58c68700d9b5e70ded
dominiclcp <dlcphk@gmail.com>
2024-06-10 15:41:12 +0200

    Update regen_cool.py

--- regen_cool.py
+++ regen_cool.py
@@ -1,8 +1,28 @@
 import numpy as np
+import CoolProp.CoolProp as CP
 
-#############################
-#______input parameters_____#
-#############################
-T_c = 3300 #combustion temperature [K]
-T_wall0 = 293 #initial wall temperature [K]
-T_cool0 = 100 #initial fuel temperature [K]
+fluid = 'CARBONMONOXIDE'
+pressure_at_critical_point = CP.PropsSI(fluid,'pcrit')
+print(pressure_at_critical_point)
+
+# #############################
+# #______input parameters_____#
+# #############################
+# T_c = 3300 #combustion temperature [K] #IMPORT FROM PREV FILE?
+# T_wall0 = 293 #initial wall temperature [K]
+# T_f0 = 100 #initial fuel temperature [K]
+# gamma_c = #import from other file 
+# mach_c = #import from other file 
+# Pr_c = #import from other file
+# mdot_c = #import from other file '
+# mdot_f = #import from other file
+# k_c = #import from other file 
+# Cp_c = #import from other file
+# D_c = 
+# eta_c = 
+# Pr_f = 
+
+
+
+
+
