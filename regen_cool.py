5fc2fcd026ebce12add3cdd2338b497609872922
dominiclcp <dlcphk@gmail.com>
2024-06-10 14:39:02 +0200

    added file for regen cooling

---
+++ regen_cool.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#############################
+#______input parameters_____#
+#############################
+T_c = 3300 #combustion temperature [K]
+T_wall0 = 293 #initial wall temperature [K]
+T_cool0 = 100 #initial fuel temperature [K]
